0f552d2c373ceaafe8495d01d808caf3fe44d4c2
DavidGL99 <dgl335@inlumine.ual.es>
2022-04-27 16:58:59 +0200

    Route to add a list of car models

--- backend/src/main/java/com/example/demo/Entity/Car.java
+++ backend/src/main/java/com/example/demo/Entity/Car.java
@@ -1,11 +1,13 @@
-package com.example.demo.Entity;
+package com.example.demo.entity;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
@@ -26,6 +28,7 @@ public class Car {
     @NotBlank(message = "carroceria is mandatory")
     private String carroceria;
 
+    @Lob
     @NotBlank(message = "imagen is mandatory")
     private String imagen;
 
