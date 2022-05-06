68cf433b29fdbb693e5763a103fbb45150aa6115
Juandi <joa102@inlumine.ual.es>
2022-05-06 18:58:30 +0200

    change folder main menu

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
-import { NavComponent } from './shared/nav/nav.component';
+import { NavComponent } from './nav/nav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatSidenavModule } from '@angular/material/sidenav';
