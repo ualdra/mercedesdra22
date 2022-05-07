e0699b308f4ab9c22a44271f07e6dbaf54a393e0
DavidGL99 <dgl335@inlumine.ual.es>
2022-05-07 13:15:43 +0200

    service to retrive the car-model information

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -37,6 +38,7 @@ import { MotoresComponent } from './motores/motores.component';
     MatMenuModule,
     MatButtonModule,
     MatButtonToggleModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
