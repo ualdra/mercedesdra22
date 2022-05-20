0f8fbfd4fc2e2c8c950ac2bb9384b0a92e2972c1
it557 <it557@inlumine.ual.es>
2022-05-20 18:41:20 +0200

    Cambios

--- frontend/src/app/configuracion-tarjeta/configuracion-tarjeta.component.ts
+++ frontend/src/app/configuracion-tarjeta/configuracion-tarjeta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CarService } from '../car.service';
 
 @Component({
@@ -17,10 +17,10 @@ export class ConfiguracionTarjetaComponent implements OnInit {
     console.log("Prueeeeba")
    console.log(this.getConfigurationsList());
   }
-  nombre="Cuero"  //Insertar el nombre s
-  descripcion="Tapizado en cuero marrón " //Insertar la descripción
-  precio="26570 €" //insertar el precio
-  imagen="https://www.polipiel.com/server/Portal_0007615/img/products/solar-color-marron-oscuro_995892.jpg" //Aqui colocar la imagen
+  @Input() nombre="Cuero"  //Insertar el nombre s
+  @Input() descripcion="Tapizado en cuero marrón " //Insertar la descripción
+  @Input() precio="26570 €" //insertar el precio
+  @Input() imagen="https://www.polipiel.com/server/Portal_0007615/img/products/solar-color-marron-oscuro_995892.jpg" //Aqui colocar la imagen
   redireccion="" //Sitio o imagen que devuelve al hacer click sobre la tarjeta
 
 
--- frontend/src/app/tapiceria/tapiceria.component.ts
+++ frontend/src/app/tapiceria/tapiceria.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { CarService } from '../car.service';
+import { ConfiguracionTarjetaComponent } from '../configuracion-tarjeta/configuracion-tarjeta.component';
 @Component({
   selector: 'app-tapiceria',
   templateUrl: './tapiceria.component.html',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TapiceriaComponent implements OnInit {
 
-  constructor() { }
-  imagen="https://w0.peakpx.com/wallpaper/762/921/HD-wallpaper-2019-mercedes-benz-a-class-edition-1-upholstery-dinamica-microfibre-artico-man-made-leather-interior-car.jpg"
+  constructor(    private carService: CarService) { }
+/*
+   */
+
+  tapicerias:ConfiguracionTarjetaComponent[] = [];
+ 
+  @Input() imagen="https://w0.peakpx.com/wallpaper/762/921/HD-wallpaper-2019-mercedes-benz-a-class-edition-1-upholstery-dinamica-microfibre-artico-man-made-leather-interior-car.jpg"
+  
   ngOnInit(): void {
+    const tapiceria1 = new ConfiguracionTarjetaComponent(this.carService);
+  /* tapiceria2 = new ConfiguracionTarjetaComponent();
+   tapiceria3 = new ConfiguracionTarjetaComponent();
+   tapiceria4 = new ConfiguracionTarjetaComponent();
+   tapiceria5 = new ConfiguracionTarjetaComponent();
+   tapiceria6 = new ConfiguracionTarjetaComponent();*/
+   this.tapicerias.push(tapiceria1)
+    this.getTapiceria()
   }
 
+  getTapiceria(){
+    console.log(this.carService.getTapiceria())
+  }
 }
