23f8b38ec3a491f5c7ea233fd41b0026900fe11d
JORGE MORA CAMPOY <jmc617@inlumine.ual.es>
2022-05-26 18:12:17 +0200

    protected

---
+++ frontend/src/app/asiento/asiento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-asiento',
+  templateUrl: './asiento.component.html',
+  styleUrls: ['./asiento.component.scss']
+})
+export class AsientoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
