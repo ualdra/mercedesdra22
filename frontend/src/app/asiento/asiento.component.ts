6a400a88885907e3aafb4245615c054a0ca2a8a0
Juandi <joa102@inlumine.ual.es>
2022-05-26 18:55:25 +0200

    Generate component asiento

--- frontend/src/app/asiento/asiento.component.ts
+++ frontend/src/app/asiento/asiento.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-asiento',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./asiento.component.scss']
 })
 export class AsientoComponent implements OnInit {
+  @Input() nombre = "Paquete de tapizado de cuero";
+  @Input() precio = "1.851,30 €";
+  @Input() caracteristicas = ["Cuero marron", "Cuero negro"];
+  @Input() imagen = "https://assets.oneweb.mercedes-benz.com/bbd/images/v1/5142/6/67/dc3d414c4d1a946b2e0ba724e78809137851b.jpg";
+
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  seleccionar():void{
+    console.log("SELECCIONADO");
   }
 
 }
