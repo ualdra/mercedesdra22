4fb43a5fc1a30f59957fc9714042c52ae2c5670e
JORGE MORA CAMPOY <jmc617@inlumine.ual.es>
2022-06-02 14:22:57 +0200

    app finished

--- frontend/src/app/garage/garage.component.ts
+++ frontend/src/app/garage/garage.component.ts
@@ -1,19 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { configuraciones } from '../configuracion';
 
 @Component({
   selector: 'app-garage',
   templateUrl: './garage.component.html',
-  styleUrls: ['./garage.component.scss']
+  styleUrls: ['./garage.component.scss'],
 })
 export class GarageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  garageImage = "assets/garage.png";
-  configuraciones: any[] = [];
-
-  ngOnInit(): void {
-
-  }
-
+  garageImage = 'assets/garage.png';
+  configuracionesGaraje = configuraciones;
+  ngOnInit(): void {}
 }
